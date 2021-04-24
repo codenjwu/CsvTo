b49dedd1cfb380731528cfaebdaa7f0206e86ab8
aaronwuus <aaron.wu.us@gmail.com>
2021-04-23 22:13:03 -0500

    add demo

--- CsvTo/CsvReverseConverter.cs
+++ CsvTo/CsvReverseConverter.cs
@@ -5,14 +5,47 @@ using System.Linq;
 
 namespace CsvTo
 {
-    public class CsvToDatatableReverse
+    public class CsvReverseConverter
     {
-        public DataTable ConvertFromFile(string filePath, bool hasHeader = false, string delimiter = ",", string escape = "\"")
+        string _filePath = null;
+        Stream _fileStream;
+        bool _hasHeader;
+        string _delimiter;
+        string _escape;
+
+        public CsvReverseConverter(string filePath, bool hasHeader = false, string delimiter = ",", string escape = "\"")
         {
-            CsvReverseHandler handler = new CsvReverseHandler(filePath, delimiter, escape);
+            _filePath = filePath;
+            _hasHeader = hasHeader;
+            _delimiter = delimiter;
+            _escape = escape;
+        }
+        public CsvReverseConverter(Stream fileStream, bool hasHeader = false, string delimiter = ",", string escape = "\"")
+        {
+            _fileStream = fileStream;
+            _hasHeader = hasHeader;
+            _delimiter = delimiter;
+            _escape = escape;
+        }
+        public DataTable ToDataTable()
+        {
+            if (!string.IsNullOrWhiteSpace(_filePath))
+                return ToDataTableFromFile();
+            return ToDataTableFromStream();
+        }
+        public IEnumerable<string[]> ToCollection()
+        {
+            if (!string.IsNullOrWhiteSpace(_filePath))
+                return ToCollectionFromFile();
+            return ToCollectionFromStream();
+        }
+
+        DataTable ToDataTableFromFile()
+        {
+            CsvReverseHandler handler = new CsvReverseHandler(_filePath, _delimiter, _escape);
             DataTable dt = new DataTable();
             var er = handler.GetEnumerator();
-            if (hasHeader)
+            if (_hasHeader)
             {
                 var tmpQueue = new Queue<string[]>();
                 if (er.MoveNext())
@@ -61,12 +94,12 @@ namespace CsvTo
             }
             return dt;
         }
-        public DataTable ConvertFromStream(Stream fileStream, bool hasHeader = false, string delimiter = ",", string escape = "\"")
+        DataTable ToDataTableFromStream()
         {
-            CsvReverseHandler handler = new CsvReverseHandler(fileStream, delimiter, escape);
+            CsvReverseHandler handler = new CsvReverseHandler(_fileStream, _delimiter, _escape);
             DataTable dt = new DataTable();
             var er = handler.GetEnumerator();
-            if (hasHeader)
+            if (_hasHeader)
             {
                 var tmpQueue = new Queue<string[]>();
                 if (er.MoveNext())
@@ -114,6 +147,71 @@ namespace CsvTo
                 }
             }
             return dt;
+        }
+
+        IEnumerable<string[]> ToCollectionFromFile()
+        {
+            CsvReverseHandler handler = new CsvReverseHandler(_filePath, _delimiter, _escape);
+            if (!_hasHeader)
+            {
+                foreach (var item in handler)
+                {
+                    var elements = handler.Parser.Split(item);
+
+                    if (!elements.All(e => string.IsNullOrWhiteSpace(e)))
+                    {
+                        yield return elements;
+                    }
+                }
+            }
+            else
+            {
+                var tmpQueue = new Queue<string[]>();
+                var er = handler.GetEnumerator();
+                while (er.MoveNext())
+                {
+                    var elements = handler.Parser.Split(er.Current);
+
+                    if (!elements.All(e => string.IsNullOrWhiteSpace(e)))
+                    {
+                        tmpQueue.Enqueue(elements);
+                        if (tmpQueue.Count > 1)
+                            yield return tmpQueue.Dequeue();
+                    }
+                }
+            }
+        }
+        IEnumerable<string[]> ToCollectionFromStream()
+        {
+            CsvReverseHandler handler = new CsvReverseHandler(_fileStream,_delimiter, _escape);
+            if (!_hasHeader)
+            {
+                foreach (var item in handler)
+                {
+                    var elements = handler.Parser.Split(item);
+
+                    if (!elements.All(e => string.IsNullOrWhiteSpace(e)))
+                    {
+                        yield return elements;
+                    }
+                }
+            }
+            else
+            {
+                var tmpQueue = new Queue<string[]>();
+                var er = handler.GetEnumerator();
+                while (er.MoveNext())
+                {
+                    var elements = handler.Parser.Split(er.Current);
+
+                    if (!elements.All(e => string.IsNullOrWhiteSpace(e)))
+                    {
+                        tmpQueue.Enqueue(elements);
+                        if (tmpQueue.Count > 1)
+                            yield return tmpQueue.Dequeue();
+                    }
+                }
+            }
         }
     }
 }
